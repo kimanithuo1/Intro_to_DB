a34c1280154627a1a4b43b3d2b946d695cc683ff
kimanithuo1 <jkimanithuo@gmail.com>
2025-08-16 18:07:30 +0300

    added task_2.sql to create tables in alx book store in my MySQL server

---
+++ task_2.sql
@@ -0,0 +1,46 @@
+
+
+USE alx_book_store;
+
+CREATE TABLE IF NOT EXISTS Authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+) ENGINE=INNODB DEFAULT CHARSET=UTF8MB4;
+
+CREATE TABLE IF NOT EXISTS Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT NOT NULL,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    CONSTRAINT fk_books_author FOREIGN KEY (author_i) REFERENCES authors(author_id) 
+        ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=INNODB DEFAULT CHARSET=UTF8MB4;
+
+CREATE TABLE IF NOT EXISTS Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT
+) ENGINE=INNODB DEFAULT CHARSET=UTF8MB4;
+
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT NOT NULL,
+    order_date DATE NOT NULL,
+    CONSTRAINT fk_orders_customer FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+        ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=INNODB DEFAULT CHARSET=UTF8MB4;
+
+CREATE TABLE IF NOT EXISTS Order_details (
+    order_details_id INT PRIMARY KEY,
+    order_id  INT NOT NULL,
+    book_id INT NOT NULL,
+    quantity DOUBLE NOT NULL,
+    INDEX idx_order_details_order_id (order_id),
+    INDEX idx_order_details_book_id (book_id),
+    CONSTRAINT fk_order_details_order FOREIGN KEY (order_id) REFERENCES Orders(order_id) 
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_order_details_book FOREIGN KEY (book_id) REFERENCES Books(book_id) 
+        ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=INNODB DEFAULT CHARSET=UTF8MB4;
