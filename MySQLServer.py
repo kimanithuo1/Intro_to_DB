7b203e326115a5995fb97420df78004f829ba86a
kimanithuo1 <jkimanithuo@gmail.com>
2025-08-16 13:50:58 +0300

    added python script to create the database in my MySQL server

---
+++ MySQLServer.py
@@ -0,0 +1,33 @@
+#importing libraries
+
+import os
+import sys
+from mysql.connector import connect, Error
+
+
+#database name
+DB_NAME = "alx_book_store"
+
+#
+def main():
+    host = os.getenv("MYSQL_HOST", "localhost")
+    user = os.getenv("MYSQL_USER", "root")
+    password = os.getenv("Mukangu!994", "")
+    port = int(os.getenv("MYSQL_PORT", "3306"))
+
+    conn = None
+    try:
+        conn = connect(host=host, user=user, password=password, port=port)
+        conn.autocommit = True
+        with conn.cursor() as cur:
+            cur.execute(f"CREATE DATABASE IF NOT EXISTS `{DB_NAME}`")
+        print(f"Database '{DB_NAME}' created successfully!")
+    except Error as e:
+        print(f"ERROR: Could not create database '{DB_NAME}'. Details: {e}")
+        sys.exit(1)
+    finally:
+        if conn is not None and conn.is_connected():
+            conn.close()
+
+if __name__ == "__main__":
+    main()
