d7f5788065e496138e21d7434db6e0183a34c972
kimanithuo1 <jkimanithuo@gmail.com>
2025-08-18 09:11:25 +0300

    added script to insert more than one row in to the databased

--- task_6.sql
+++ task_6.sql
@@ -2,7 +2,8 @@
 USE alx_book_store;
 
 INSERT INTO customers (customer_id, name, email, address)
+
 VALUES 
-(2, "Jane Doe", "jane.doe@email.com", "124 Happiness Ave."),
+(2, "Jane Doe", "jane.doe@email.com", "124 Happiness  Ave."),
 (3, "Alice Smith", "alice.smith@email.com", "56 Peace Street"),
 (4, "Bob Johnson", "bob.johnson@email.com", "78 Unity Road");
